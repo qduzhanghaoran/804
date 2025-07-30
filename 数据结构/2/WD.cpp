bf2f230963cbbcd150cdc556991416eb6bbcbaf2
qduzhanghaoran <qduzhanghaoran@163.com>
2025-07-30 21:16:23 +0800

    7.30update

--- 数据结构/2/WD.cpp
+++ 数据结构/2/WD.cpp
@@ -133,7 +133,7 @@ bool Exchang(ELemType A[], int m, int n, int arraysize)
     Reverse_array(A, n, m + n - 1, arraysize);
 }
 
-bool SearchExchangInsert(ELemType A[], ELemType x, int n)
+bool SearchExchangeInsert(ELemType A[], ELemType x)
 {
     // 数组中元素有序递增，最高效查找x，如果找到和后继元素交换，否则插入x保持递增
     /*二分查找*/
@@ -163,4 +163,141 @@ bool SearchExchangInsert(ELemType A[], ELemType x, int n)
         A[high + 1] = x;
     }
     return true;
+}
+
+int M_Search_On(int A[], int B[], int n)
+{
+
+    // 对两个长度为n的序列按照从小到大排序依次访问
+    // 并不是先合并
+    int ans;
+
+    int a, b = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (A[a] <= B[b])
+        {
+            ans = A[a++];
+        }
+        else
+        {
+            ans = A[b++];
+        }
+    }
+    return ans;
+}
+
+int M_Search_logn(int A[], int B[], int n)
+{
+    // 折半查找
+    /*A的中位数为mida，B的中位数为midb.
+    如果mida==midb 中位数为mida=midb
+    如果mida<midb 中位数位于mida->midb之间 舍弃A中较小的一般 B中较大的一半
+    同理mida>midb
+    并且每个数组都设置两对首尾指针
+    */
+
+    int leftA, leftB = 0;
+    int rightA, rightB = n - 1;
+    int midA, midB;
+    while (leftA != rightA || leftB != rightB)
+    {
+        midA = (leftA + rightA) / 2;
+        midB = (leftB + rightB) / 2;
+
+        if (A[midA] == B[midB])
+            return A[midA];
+        if (A[midA] < B[midB])
+        {
+            if ((rightA - lefeA + 1) % 2 != 0)
+            {
+                leftA = midA;
+                rightB = midB;
+            }
+            else
+            {
+                leftA = midA + 1;
+                rightB = midB;
+            }
+        }
+        else
+        {
+            if ((rightA - lefeA + 1) % 2 != 0)
+            {
+                rightA = midA;
+                leftB = midB;
+            }
+            else
+            {
+                rightA = midA;
+                rleftB = midB + 1;
+            }
+        }
+    }
+
+    return A[midA] < B[midB] ? A[midA] : B[midB]
+}
+
+int Majority(int A[], int n)
+{
+    // 数组中出现次数大于n/2的为主元素
+    // 查找数组中的主元素
+    // 使用count来计数，假设c为主元素遇到重复加1，不重复-1，最终肯定会保留计数
+    // 其他非主元素由于小于n/2最终肯定会重新计
+    // 也可以使用辅助数组
+    int c, count = 1;
+    c = A[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (A[i] == c)
+            count++;
+        else
+        {
+            if (count > 0)
+                count--;
+            else
+            {
+                c = A[i];
+                count = 1;
+            }
+        }
+    }
+
+    if (count > 0)
+    {
+        count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (A[i] == c)
+                count++;
+        }
+    }
+    if (count > n / 2) // 需要判断是否为n/2 例如 A=[0,5,5,3,5,1,5,7]
+        return c;
+    else
+        return -1;
+}
+
+int findMissMid(int A[], int n)
+{
+    // 寻找从未出现的最小正整数
+    // 使用空间换时间
+    // 使用辅助数组size=n即可
+
+    int temp[n + 1];
+
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] >= 0 && A[i] <= n)
+        {
+            temp[A[i]] = 1;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (temp[i] == 0)
+        {
+            return i;
+        }
+    }
 }
